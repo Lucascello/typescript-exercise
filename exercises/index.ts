f77da8e5cf4cd3b7466fa148781e8c845443235f
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-09 13:29:17 +0100

    ts done

--- exercises/index.ts
+++ exercises/index.ts
@@ -73,32 +73,41 @@ const getAlbumsByArtistName = (arg: string): Album[] => {
 
 // To find the objects of the Album type, use the getAlbumsByArtistName function that you wrote.
 //
-const getArtistAndAlbumsByArtistName = (arg: string): (Album | Artist)[] => {
-    let album: (Album | Artist)[] = [];
+type AlbumOrArtist = Artist | Album;
+
+function getArtistAndAlbumsByArtistName(artist: string): AlbumOrArtist[] {
+    let artistArray: AlbumOrArtist[] = [];
     for (let i = 0; i < artists.length; i++) {
-        if (arg === artists[i].name) {
-            album.push(artists[i]);
+        if (artists[i].name === artist) {
+            // console.log(albums[i]);
+            artistArray.push(artists[i]);
+            break;
         }
     }
-    album.push(...getAlbumsByArtistName(arg));
-    return album;
-}; //
+    artistArray.push(...getAlbumsByArtistName(artist));
+    return artistArray;
+}
+//
 // 4 - Write a getArtistWithAlbumNames function that expects to receive an array that contains a single object of the Artist type and any number of objects of the Album type (the idea is that the caller would obtain this array by calling the getArtistAndAlbumsByArtistName function). The getArtistWithAlbumNames function should return an object of a new type that has all the same properties as objects of the Artist type but with the addition of an albumNames property, whose value is an array of strings. The strings in the albumNames array should be extracted from the objects of the Album type in the array that was passed to getArtistWithAlbumNames.
 //
-const getArtistWithAlbumNames = function (getArtistAndAlbumsByArtistName: function {
-    
-}) {
-    let newAlbum = {
-        name: "",
-        category: "artist",
-        albumNames: [],
-    };
-    getArtistAndAlbumsByArtistName.forEach(function (newObj: object) {
-        if (newObj.category === "artist") {
-            newAlbum.name = newObj.name;
+interface ArtistWithAlbums extends Artist {
+    albumNames: string[];
+}
+
+function getArtistWithAlbumNames(
+    getArtistAndAlbums: AlbumOrArtist[]
+): ArtistWithAlbums {
+    let artistRes: Artist;
+    let albumNames: string[] = [];
+    //artistRes.name = getArtistAndAlbums[0].name;
+    //artistRes.category = getArtistAndAlbums[0].category as "artist";
+    for (let i = 0; i < getArtistAndAlbums.length; i++) {
+        if ("artistName" in getArtistAndAlbums[i]) {
+            albumNames.push(getArtistAndAlbums[i].name);
         } else {
-            newAlbum.albumNames.push(newObj);
+            artistRes = getArtistAndAlbums[i] as Artist;
         }
-    });
-    return newAlbum;
-};
+    }
+
+    return { ...artistRes, albumNames };
+}
